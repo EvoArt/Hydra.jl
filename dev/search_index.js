95a19e4819232359a5a90e95f17e86b3c3961364
Documenter.jl <documenter@juliadocs.github.io>
2021-08-24 14:25:55 +0000

    build based on 30bc0d7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Hydra","category":"page"},{"location":"#Hydra","page":"Home","title":"Hydra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Hydra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Hydra]","category":"page"}]
+}
