d66becc272adad7c68abb8127d7dd1358e35229b
Arthur <arn203@exeter.ac.uk>
2021-08-27 11:29:20 +0100

    re-export some distance measures

--- src/Hydra.jl
+++ src/Hydra.jl
@@ -3,7 +3,19 @@ module Hydra
 using Random, LinearAlgebra, Distances, NamedArrays, StatsModels,Statistics
 include("hydra2.jl")
 
-export hydra2, permute, @formula
+export hydra2, 
+    permute, 
+    Euclidean,
+    SqEuclidean,
+    PeriodicEuclidean,
+    Cityblock,
+    TotalVariation,
+    Chebyshev,
+    Minkowski,
+    Jaccard,
+    BrayCurtis,
+    RogersTanimoto,
+    @formula
 
 
 end
